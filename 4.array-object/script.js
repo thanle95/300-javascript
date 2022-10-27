315a3f1584df6efec928393069e9012a0dfde5e4
thanle95 <lengocthan95@gmail.com>
2022-10-27 10:30:28 +0700

    4.array-object

---
+++ 4.array-object/script.js
@@ -0,0 +1,61 @@
+const users = [
+  {
+    name: "Dũng",
+    age: 22,
+    salary: 500,
+    hasAJob: true,
+  },
+  {
+    name: "Hoàng",
+    age: 25,
+    salary: 1000,
+    hasAJob: true,
+  },
+  {
+    name: "Lan",
+    age: 30,
+    hasAJob: false,
+  },
+];
+
+
+// 1
+users.push({ name: "An", age: 30, salary: 2000, hasAJob: true });
+console.log(users);
+// 2
+users.unshift({ name: "An", age: 30, salary: 2000, hasAJob: true });
+console.log(users);
+// 3
+users.sort((a,b)=> a.age - b.age)
+console.log(users)
+users.sort((a,b)=> b.age - a.age)
+console.log(users)
+// 4
+const staffs = users.filter(f=> f.hasAJob)
+console.log(staffs)
+// 5
+const newStaffs = [...staffs]
+console.log(newStaffs)
+// 6
+const students = [
+    {
+        name: "Adam",
+        age: 18,
+      },
+      {
+        name: "Eva",
+        age: 19,
+      },
+]
+console.log(students)
+// 7
+const adam = students.find(f=> f.name === "Adam")
+adam.university = "CMC"
+console.log(adam)
+console.log(students)
+// 8
+const newUsers = [...newStaffs, ...students]
+// 9
+console.log(
+  newUsers.map((m) => m?.salary ?? 0).reduce((sum, num) => sum + num, 0)
+);
